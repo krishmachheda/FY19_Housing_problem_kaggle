623dd548bfaa03a9b36c74bf473ae436c16a8b4e
krishmachheda <40134182+krishmachheda@users.noreply.github.com>
2018-06-11 00:51:46 +0530

    data pre-processing complete

--- Housing project.R
+++ Housing project.R
@@ -1,3 +1,6 @@
+## ------------------------------------------------------------- ##
+## -------------------- BOSTON HOUSING PROJECT --------------------- ##
+## ------------------------------------------------------------- ##
 setwd("D:/kaggle/Housing Project")
 
 # reading the datasets: 
@@ -7,6 +10,9 @@ test_data<- read.csv("test.csv", stringsAsFactors = F)
 # understading the dataset: 
 str(train_data)
 
+## ------------------------------------------------------------- ##
+## -------------------- DATA PREPROCESSING --------------------- ##
+## ------------------------------------------------------------- ##
 # replacing the missing values of various char columns by 'not present' 
 # then converting it to factors. 
 for (i in 1:length(names(train_data))){
@@ -17,12 +23,18 @@ for (i in 1:length(names(train_data))){
 }
 
 # converting some character factor columns into numeric factors
-# doing it separately for each columns as the treatment for each of it is different
+train_data$CentralAir<- as.numeric(train_data$CentralAir)
 
+# coverting some columns to factors: 
+train_data$MSSubClass<- as.factor(train_data$MSSubClass)
+train_data$YearBuilt<- as.factor(train_data$YearBuilt)
+train_data$MoSold<- as.factor(train_data$MoSold)
+train_data$YrSold<- as.factor(train_data$YrSold)
 
-for (i in 1:length(names(train_data))){
-  if(class(train_data[,i])=="integer"){
-    train_data[,i][is.na(train_data[,i])==T]<-"Not Present"
-    train_data[,i]<-as.factor(train_data[,i])
-  }
-}
+# creating a new variable using YearRemodAdd and garagreyrbuilt
+train_data$NumYearRemodAdd<- as.numeric(format(Sys.Date(), "%Y")) - train_data$YearRemodAdd
+train_data$GarageYrBltSince<- as.numeric(format(Sys.Date(), "%Y")) - train_data$GarageYrBlt
+
+## ------------------------------------------------------------- ##
+## -------------------- FEATURE IMPORTANCE --------------------- ##
+## ------------------------------------------------------------- ##
