525c2cc2b405630f673d1c79914037743e48809f
krishmachheda <40134182+krishmachheda@users.noreply.github.com>
2018-06-12 12:31:40 +0530

    feature importance/feature selection

--- Housing project.R
+++ Housing project.R
@@ -35,6 +35,34 @@ train_data$YrSold<- as.factor(train_data$YrSold)
 train_data$NumYearRemodAdd<- as.numeric(format(Sys.Date(), "%Y")) - train_data$YearRemodAdd
 train_data$GarageYrBltSince<- as.numeric(format(Sys.Date(), "%Y")) - train_data$GarageYrBlt
 
+# replacing NAs with mean/required values
+train_data$LotFrontage[is.na(train_data$LotFrontage)==T]<- mean(train_data$LotFrontage, na.rm = T)
+train_data$GarageYrBltSince[is.na(train_data$GarageYrBltSince)==T]<- 0
+train_data$MasVnrArea[is.na(train_data$MasVnrArea)==T]<- 0
+
+#removing some variables from the database 
+train_data<- subset(train_data, select = -c(GarageYrBlt,YearBuilt))
+
 ## ------------------------------------------------------------- ##
 ## -------------------- FEATURE IMPORTANCE --------------------- ##
 ## ------------------------------------------------------------- ##
+
+###------------ Random Forest --------------###
+# building random forest regression using all variables - except Id column
+library(randomForest)
+reg_mod1<- randomForest(SalePrice ~ .-Id, data = train_data, type = 'regression')
+
+# getting the importance of variables from the model
+variable_imp<- importance(reg_mod1)
+
+# coverting the matrix into a data frame
+varImportance <- data.frame(Variables = row.names(variable_imp), 
+                            Importance = round(variable_imp[ ,'IncNodePurity'],2))
+
+# ranking the varibales based on importance:
+library(dplyr)
+variable_rank<- varImportance %>%
+  mutate(Rank = paste0(dense_rank(desc(Importance))))
+variable_rank$Rank = as.numeric(variable_rank$Rank)
+###--- So now we have variable ranks using random forest
+###--- We will now try two more methods to get ranks and then conclude the important varibales
